6583d8d48bba716c66df7b96a493f322ac21ed8b
giuseppe sorrentino <giuseppesorrentino11290@gmail.com>
2023-02-03 15:53:27 +0100

    Initial commit

---
+++ src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
@@ -0,0 +1,16 @@
+package co.develhope.firstapi4.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeaderController {
+
+    @GetMapping("/headers")
+    public String getHost(@RequestHeader(name = "host") String host){
+        return host;
+    }
+
+
+}
